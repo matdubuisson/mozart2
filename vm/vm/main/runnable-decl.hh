5c4881c9a8f47ddc6df871b1dd9ee7b12cd23cc8
Matteo Dubuisson <matteo.dubuisson@student.uclouvain.be>
2026-04-11 16:09:18 +0200

    Comment runnable-decl.hh

--- vm/vm/main/runnable-decl.hh
+++ vm/vm/main/runnable-decl.hh
@@ -45,41 +45,95 @@ enum ThreadPriority {
 // IntermediateState //
 ///////////////////////
 
+// TO-COMPLETE : the comments for IntermediateState needs some refactoring
 class IntermediateState {
 private:
   typedef VMAllocatedList<UnstableNode> List;
 public:
   typedef List::iterator CheckPoint;
 public:
+  /**
+   * Instantiates a new intermediate state
+   * @param vm The virtual machine
+   */
   inline
   explicit IntermediateState(VM vm);
 
+  /**
+   * Instantiates a new intermediate state
+   * Copies the list of unstable nodes of the provided intermediate state
+   * @param vm The virtual machine
+   * @param gr The graph replicator
+   * @param from A reference on another intermediate state
+   */
   inline
   IntermediateState(VM vm, GR gr, IntermediateState& from);
 
+  /**
+   * Gets the last element of the list of unstable nodes as check point
+   * @param vm The virtual machine
+   * @returns An iterable on the check point (unstable node)
+   */
   CheckPoint makeCheckPoint(VM vm) {
     return _last;
   }
 
+  /**
+   * Removes all nodes after the checkpoint from the list
+   * @param vm The virtual machine
+   * @param checkPoint An iterator on the list
+   */
   inline
   void reset(VM vm, CheckPoint checkPoint);
 
+  /**
+   * Removes all nodes after the checkpoint from the list
+   * @param vm The virtual machine
+   */
   inline
   void reset(VM vm);
 
+  /**
+   * Tries to fetch the next checkpoint in the list
+   * @param vm The virtual machine
+   * @param identity The identity name
+   * @param args The native values
+   * @returns True if the fetch succeeded (last checkpoint matched identity(args))
+   *          else false or because the end of list is reached
+   * @note A tuple is formed as identity(args)
+   */
   template <typename... Args>
   inline
   bool fetch(VM vm, const char* identity, Args... args);
 
+  /**
+   * Stores an new tuple as a check point at the end of the list
+   * @param vm The virtual machine
+   * @param identity The identity name
+   * @param args The native rvalues
+   * @note A tuple is formed as identity(args)
+   */
   template <typename... Args>
   inline
   void store(VM vm, const char* identity, Args&&... args);
 
+  /**
+   * Resets and stores the check point defined by a tuple
+   * @param vm The virtual machine
+   * @param checkpoint The checkpoint in the list
+   * @param identity The identity name
+   * @param args The native rvalues
+   * @note A tuple is formed as identity(args)
+   */
   template <typename... Args>
   inline
   void resetAndStore(VM vm, CheckPoint checkPoint, const char* identity,
                      Args&&... args);
 
+  /**
+   * Resets the cursor at the beginning of the list
+   * @param vm The virtual machine
+   */
   inline
   void rewind(VM vm);
 private:
@@ -93,38 +147,84 @@ private:
 
 class Runnable {
 public:
+  /**
+   * Initializes a thread defined as a VM, a space and a priority level
+   * @param vm The virtual machine
+   * @param space The space for the thread
+   * @param priority The priority level
+   */
   inline
   Runnable(VM vm, Space* space, ThreadPriority priority = tpMiddle);
 
+  /**
+   * Initializes a thread from an other thread
+   * @param gr The graph replicator to copy the space of the parent thread
+   * @param from A reference on the parent thread to copy
+   */
   inline
   Runnable(GR gr, Runnable& from);
 
+  /** Gets the space of the thread */
   Space* getSpace() {
     return _space;
   }
 
+  /** Gets the priority of the thread */
   ThreadPriority getPriority() { return _priority; }
 
+  /**
+   * Sets the priority of the thread
+   * @param priority The thread priority level
+   * @note It might force the thread pool to reschedule the thread
+   */
   inline
   void setPriority(ThreadPriority priority);
 
+  /**
+   * Runs the thread
+   */
   virtual void run() = 0;
 
+  /** Tells if the thread is terminated */
   bool isTerminated() { return _terminated; }
+
+  /** Tells if the thread is runnable */
   bool isRunnable() { return _runnable; }
 
+  /**
+   * Resumes the thread
+   * @param skipSchedule Decides if the thread has to be scheduled in the thread pool or not
+   * @note The thread has to be alive, non-terminated and non-runnable
+   */
   inline
   virtual void resume(bool skipSchedule = false);
 
+  /**
+   * Suspends the thread
+   * @param skipUnschedule Decides if the thread has to be unscheduled in the thread pool or not
+   * @note The thread has to be alive, non-terminated and runnable
+   */
   inline
   virtual void suspend(bool skipUnschedule = false);
 
+  /**
+   * Suspends the thread on a dataflow variable
+   * @param vm The virtual machine
+   * @param variable A rich node representing the suspending variable
+   * @param skipUnschedule Decides if the thread has to be unscheduled in the thread pool or not
+   * @note The variable must be transient and not a failed value
+   */
   inline
   void suspendOnVar(VM vm, RichNode variable, bool skipUnschedule = true);
 
+  /**
+   * Kills/Disposes the thread
+   * @note The thread must not be dead or terminated
+   */
   inline
   virtual void kill();
 
+  /** Should be never called */
   inline
   virtual void injectException(StableNode* exception) {
     /* We cannot come here because there's no way the Oz code has gained a
@@ -133,39 +233,77 @@ public:
     assert(false);
   }
 
+  /** Gets raise on block */
   bool getRaiseOnBlock() {
     return _raiseOnBlock;
   }
 
+  /** Sets raise on block */
   void setRaiseOnBlock(bool value) {
     _raiseOnBlock = value;
   }
 
+  /** Gets intermediate state */
   IntermediateState& getIntermediateState() {
     return _intermediateState;
   }
 
+  /**
+   * Prepares the thread for graph replication
+   */
   virtual void beforeGR() {}
+
+  /**
+   * Prepares the thread to get back to normal after graph replication
+   */
   virtual void afterGR() {}
 
+  /**
+   * Runs the garbage collector on the thread
+   * @param gc The garbage collector
+   * @returns A pointer on the replicated runnable
+   */
   virtual Runnable* gCollect(GC gc) = 0;
+
+  /**
+   * Runs the space cloner on the thread
+   * @param sc The space cloner
+   * @returns A pointer on the replicated runnable
+   */
   virtual Runnable* sClone(SC sc) = 0;
 
+  /**
+   * Runs the garbage collector on the thread
+   * @param gc The garbage collector
+   * @returns A pointer on the replicated runnable
+   */
   inline
   Runnable* gCollectOuter(GC gc);
 
+  /**
+   * Runs the space cloner on the thread
+   * @param sc The space cloner
+   * @returns A pointer on the replicated runnable
+   */
   inline
   Runnable* sCloneOuter(SC sc);
 
+  /** Restores after graph replication */
   inline
   void restoreAfterGR();
 protected:
+  /**
+   * Terminates the thread
+   * @note The thread must be alive, non-terminated and runnable
+   */
   inline
   virtual void terminate();
 
+  /** Disposes/Kills the thread */
   inline
   virtual void dispose();
 public:
+  /** Dumps the thread */
   virtual void dump() {}
 protected:
   VM vm;
@@ -194,6 +332,7 @@ private:
 
 class RunnableList {
 public:
+  /** Defines a whole iterator for a linked-list like */
   struct iterator {
   public:
     iterator(Runnable* node) : node(node) {}
@@ -224,18 +363,25 @@ public:
     Runnable* node;
   };
 public:
+  /** Initializes a runnable list */
   RunnableList() : first(nullptr), last(nullptr) {}
 
+  /** Gets the beginning of the list */
   iterator begin() {
     return iterator(first);
   }
 
+  /** Gets the end of the list */
   iterator end() {
     return iterator(nullptr);
   }
 private:
   friend class Runnable;
 
+  /**
+   * Inserts a new runnable address at the end of the list
+   * @param item The address of the runnable
+   */
   void insert(Runnable* item) {
     item->_previous = last;
     item->_next = nullptr;
@@ -248,6 +394,10 @@ private:
     last = item;
   }
 
+  /**
+   * Removes a runnable address from the beginning of the list
+   * @param item The address of the runnable
+   */
   void remove(Runnable* item) {
     if (item->_previous == nullptr)
       first = item->_next;
