3953b0a7b337f5dc30d3a008d9d42072f2a7ad5f
Matteo Dubuisson <matteo.dubuisson@student.uclouvain.be>
2026-07-08 21:28:13 +0200

    Fix garbage collected threads issue, for same thread, new memory got a new id that made threads changing ids overtime that was absolutely wrong

--- vm/vm/main/runnable.hh
+++ vm/vm/main/runnable.hh
@@ -113,7 +113,7 @@ Runnable::Runnable(VM vm, Space* space, ThreadPriority priority) :
 
 Runnable::Runnable(GR gr, Runnable& from) :
   vm(gr->vm), _intermediateState(vm, gr, from._intermediateState),
-  _replicate(nullptr), _id(_everCreatedThreadsCount++),
+  _replicate(nullptr), _id(from._id),
   _kindId(0), _generationId(0) {
 
   gr->copySpace(_space, from._space);
